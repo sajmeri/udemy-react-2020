b5a16e1c74de0bebeb41bfe075bfa5a75463b38f
unknown <shahnazajmeri@gmail.com>
2020-01-28 22:17:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from "./components/cardlist/card-list";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      monsters: [],
+      searchField:''
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+  render(){
+    const {monsters, searchField} = this.state;
+    const fiteredMonsters = monsters.filter(monster=>monster.name.includes(searchField));
+    return (
+      <div className="App">
+        <input type="search" placeholder="Search Monserr!"  onChange={
+          e => {this.setState(
+            {searchField: e.target.value}    
+            )}
+        }/>
+        <CardList monsters={fiteredMonsters} />        
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
