34ef22ad5ccc645e1adcf6cf63350b8fdb91c1c2
unknown <shahnazajmeri@gmail.com>
2020-01-30 21:04:58 -0500

    Updates

---
+++ precache-manifest.a1fa4e9327cfc3b27535edbe428ccda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13501cfa44c2a3e2c544b6b0c8f2aa4",
+    "url": "/udemy-react-2020/index.html"
+  },
+  {
+    "revision": "68403ee1044288210632",
+    "url": "/udemy-react-2020/static/css/main.cb951dab.chunk.css"
+  },
+  {
+    "revision": "7e7d3b29ba48c60b4d4b",
+    "url": "/udemy-react-2020/static/js/2.a8b7be0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/udemy-react-2020/static/js/2.a8b7be0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68403ee1044288210632",
+    "url": "/udemy-react-2020/static/js/main.3f7a447a.chunk.js"
+  },
+  {
+    "revision": "47cd428be275d4e37fff",
+    "url": "/udemy-react-2020/static/js/runtime-main.d952f9a2.js"
+  }
+]);
